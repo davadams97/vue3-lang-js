564b7200e06341c4ec78e7caefb5871f61d08f5e
David A. Adams <david.adams97@gmail.com>
2024-03-24 22:12:13 -0400

    refactor: cleanup functions
    reduce global properties provided

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
 import Lang from 'lang.js';
 
-export default {
-  install: (app, options) => {
-    const messages = options.messages || {};
-    const locale = options.locale || 'en';
-    const fallback = options.fallback || 'en';
+function createLangInstance(options) {
+  const { messages = {}, locale = 'en', fallback = 'en' } = options;
 
-    const lang = new Lang({
-      messages,
-      locale,
-      fallback
-    });
+  return new Lang({
+    messages,
+    locale,
+    fallback
+  });
+}
 
-    /** Get translation from the key */
-    const translate = (key, options) => {
-      return lang.trans(key, options);
-    };
+export const Vue3LangPlugin = {
+  install: (app, options = {}) => {
+
+    const lang = createLangInstance(options);
 
     /** Get the plural or singular form of the message */
-    const pluralTranslate = (key, plural, options) => {
+    const translate = (key, plural, options) => {
+      // If the second argument is an object, assume it's the options object
+      if (typeof plural === 'object') {
+        return lang.trans(key, plural);
+      }
+
       return lang.choice(key, plural, options);
     };
 
-    /** Is the key defined in the message source */
-    const has = (key) => {
-      return lang.has(key);
-    };
 
     app.config.globalProperties.$trans = translate;
     app.config.globalProperties.$t = translate;
 
-    app.config.globalProperties.$choice = pluralTranslate;
-    app.config.globalProperties.$tc = pluralTranslate;
-
-    app.config.globalProperties.$has = has;
-
     app.provide('$trans', translate);
     app.provide('$t', translate);
-
-    app.provide('$choice', pluralTranslate);
-    app.provide('$tc', pluralTranslate);
-
-    app.provide('$has', has);
   }
 };
